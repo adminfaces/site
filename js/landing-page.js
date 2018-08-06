e92dc3bf85b9afe355e69dbaf3995beb36aa792d
rmpestano <rmpestano@gmail.com>
2018-08-06 10:52:30 -0300

    fix scroll spy

--- js/landing-page.js
+++ js/landing-page.js
@@ -14,6 +14,7 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
+
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
